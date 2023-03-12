10a9e35d9929a4af818dcd9eda024fc33c58d2d7
cavemanloverboy <caveycool@gmail.com>
2023-03-11 21:37:33 -0800

    comment + fmt

--- graph-demo/src/main.rs
+++ graph-demo/src/main.rs
@@ -1,6 +1,11 @@
 use graph_demo::*;
 use std::io::Write;
 
+///  ┌────────────►alice──────────────┐
+///  │                                ▼
+/// dave                             bob
+///  ▲                                │
+///  └──────────────carol◄────────────┘
 fn main() {
     let nodes: Vec<GraphNode> = vec![
         GraphNode {
@@ -25,7 +30,8 @@ fn main() {
     std::fs::create_dir_all(DIR).unwrap();
     for node in nodes {
         let node_bytes = rkyv::to_bytes::<_, 256>(&node).unwrap();
-        let mut node_file = std::fs::File::create(format!("{DIR}/{}", &node.name)).unwrap();
+        let mut node_file =
+            std::fs::File::create(format!("{DIR}/{}", &node.name)).unwrap();
         node_file.write_all(&node_bytes).unwrap();
     }
 }
