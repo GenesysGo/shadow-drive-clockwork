e7606e1511df2b54761f4d91ec71ed93780fb94c
cavemanloverboy <caveycool@gmail.com>
2023-03-12 14:54:50 -0700

    add payout authority

--- programs/chain-drive/src/constants.rs
+++ programs/chain-drive/src/constants.rs
@@ -9,3 +9,17 @@ pub mod shdw {
     #[cfg(not(feature = "mainnet"))]
     anchor_lang::declare_id!("RUNEkHeD5P8DsSpuDwxyZZKsj3T9e1ooMiaXL9H71yc");
 }
+
+pub mod payout_authority {
+    #[cfg(feature = "mainnet")]
+    anchor_lang::declare_id!("D6wZ5U9onMC578mrKMp5PZtfyc5262426qKsYJW7nT3p");
+    #[cfg(not(feature = "mainnet"))]
+    anchor_lang::declare_id!("3cZiETXADiH4spd8rdBCt5DwuVQMPuNnq8e7Ci2ky65L");
+}
+
+pub fn payout_account() -> anchor_lang::prelude::Pubkey {
+    anchor_spl::associated_token::get_associated_token_address(
+        &payout_authority::ID,
+        &shdw::ID,
+    )
+}
