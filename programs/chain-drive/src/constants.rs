368c217ddaffe987926be958e8b267a0abfa5d02
cavemanloverboy <caveycool@gmail.com>
2023-03-11 21:35:07 -0800

    add shdw mint addresses

--- programs/chain-drive/src/constants.rs
+++ programs/chain-drive/src/constants.rs
@@ -1,8 +1,11 @@
-use anchor_lang::prelude::Pubkey;
-
 pub const SDRIVE_OBJECT_PREFIX: &'static str =
     "https://shdw-drive.genesysgo.net";
 
 pub const TIME_DELAY_SECS: i64 = 0;
 
-// pub const SHDW_TOKEN: Pubkey = Pubkey::new_from_array([0; 32]);
+pub mod shdw {
+    #[cfg(feature = "mainnet")]
+    anchor_lang::declare_id!("SHDWyBxihqiCj6YekG2GUr7wqKLeLAMK1gHZck9pL6y");
+    #[cfg(not(feature = "mainnet"))]
+    anchor_lang::declare_id!("RUNEkHeD5P8DsSpuDwxyZZKsj3T9e1ooMiaXL9H71yc");
+}
